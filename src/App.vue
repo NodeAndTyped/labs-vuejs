e7b78b0a64890ff8d5a25dda2bc809fc51da532e
romain.lenzotti <rom.lenzotti@gmail.com>
2017-06-28 16:44:51 +0200

    create todo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div>
+      <!-- your compoennt -->
+    </div>
+  </div>
+</template>
+
+<script>
+// import component todo
+
+export default {
+  name: 'app',
+  components: {}
+}
+</script>
+
+<style scoped>
+.filters li.selected {
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/todos.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="todoapp">
+  </section>
+</template>
+
+<script>
+</script>
